654cacd3bba3ca1b180c20f035ed4c1a2430198f
calumtreloar <calumtreloar@gmail.com>
2024-03-15 15:02:59 +1030

    Initial Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>Strapi is the leading open-source headless CMS. 100% JavaScript / TypeScript and fully customisable.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
